a01e6f19cf037dc568ff3906050ebaa60004e9c7
tomjmcnamee <tom@tomjmcnamee.com>
2019-10-10 17:51:31 -0400

    removed console.logs,  data does NOT persist

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 class Filters extends React.Component {
   render() {
-    console.log("this is the petsArr upon Fi.ltersRender", this.props)
-    // console.log("filtes:5 this.props", this.props)
     return (
       <div className="ui form">
         <h3>Animal type</h3>
@@ -17,7 +15,6 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          {console.log("filters:19 filters.type: ", this.props.state)}
           <button className="ui secondary button" onClick={() => this.props.onFindPetsClick(this.props.state.filters.type)}>Find pets</button>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,15 +4,9 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    // debugger
-    // console.log(this.props.petsArr.pets)
-    // let filter = this.props.petsArr.filters.type
-    // let filteredPets = filter === "all" ? this.props.petsArr.pets : this.props.petsArr.pets.filter( petObj => petObj.type === filter)
     let filteredPets = this.props.petsArr.pets
     let petCardsToPrint = filteredPets.map( pet => {return <Pet key={pet.id} petObj={pet} onChangeStatus={this.props.onChangeStatus}/> })
 
-
-    // return <div className="ui cards">hi</div>
     return <div className="ui cards">{petCardsToPrint}</div>
   }
 }
